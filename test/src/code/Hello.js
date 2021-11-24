ff695e5b2a42999037b8888a40f932fcf07ee2d3
sojihwan <73238543+sojihwan@users.noreply.github.com>
2021-11-24 10:09:02 +0900

    React cond

---
+++ test/src/code/Hello.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Hello(props){
+  return <div style={{
+    color :props.color
+  }}>
+    {props.isSpecial && <b>*</b>}
+    {props.name}안녕하세요
+    {props.isSpecial && <b>*</b>}
+    </div>;
+}
+Hello.defaultProps ={
+  name: '이름없음'
+};
+export default Hello;
