400f26dd16162e2c46a5556700171a10c6463d99
sojihwan <73238543+sojihwan@users.noreply.github.com>
2021-11-24 10:29:05 +0900

    React code

--- test/src/code/InputSample.js
+++ test/src/code/InputSample.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
 
 function InputSample(){
+  const [text, setText] = useState('');
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+  const onReset = () => {
+    setText('');
+  };
+
   return(
     <div>
-      <input/>
-      <button>초기화</button>
+      <input onChange={onChange} value={text}/>
+      <button onClick={onReset}>초기화</button>
       <div>
         <b>값:</b>
-        블라블라블라
+        {text}
       </div>
     </div>
   )
